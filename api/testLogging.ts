0201360f807bb1729d243842d9919b8077501abc
prabakarankannan <50018171+prabakarankannan@users.noreply.github.com>
2024-07-29 22:14:28 +0530

    Delete api directory

--- api/testLogging.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('Logging from API route...');
-  
-  res.status(200).json({ message: 'Logging is working!' });
-}
